1bcd2275e0066cbc70e24644e3dfd158c2365770
Hardiyan Purnama <hardiyan.purnama.hp@gmail.com>
2022-09-19 22:48:24 +0700

    Promises task telah dikerjakan

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  let outputIXX = await promiseTheaterIXX();
+  let allData = outputIXX.concat(await promiseTheaterVGC());
+
+  let hitungEmosi = 0;
+  allData.forEach(perasaan => {
+    if (perasaan.hasil == emosi) {
+      hitungEmosi++;
+    }
+  });
+  return hitungEmosi;
+}
 
 module.exports = {
   promiseOutput,
